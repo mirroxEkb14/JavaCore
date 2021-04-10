8841c23ce14e2097be8abfe8732f67309619d760
user <dancho.rancho07@gmail.com>
2021-04-10 23:46:58 +0600

    module 11 is done

---
+++ src/Generics/PracticalTask/Student.java
@@ -0,0 +1,39 @@
+package Generics.PracticalTask;
+
+import java.util.Objects;
+
+public class Student {
+
+    private final String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
