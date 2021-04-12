4cf5c1dcb07fe6e7613d5e893b26c59c716923b6
user <dancho.rancho07@gmail.com>
2021-04-12 22:47:36 +0600

    linkedList running

--- src/Generics/ArrayAndLinkedListsTheory/LinkedList/Demo.java
+++ src/Generics/ArrayAndLinkedListsTheory/LinkedList/Demo.java
@@ -5,8 +5,36 @@ public class Demo {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<>();
 
+        // add to the and of the list
         linkedList.insert(1);
-        linkedList.insert(1);
+        linkedList.insert(2);
         linkedList.insert(3);
+
+        // display ('void' method)
+        linkedList.display();
+
+        // 'toString' method returns the string of all list elements
+        System.out.println("List in string format: " + linkedList.toString());
+
+        // returns the size of the list
+        System.out.println("List size -> " + linkedList.size());
+
+        // does the same as 'size()' method
+        System.out.println("List length -> " + linkedList.length());
+
+        // inserts a value at the beginning of the list
+        linkedList.insertAtFirst(0);
+        System.out.println("Inserted in the beginning: " + linkedList.toString());
+
+        // take an elements by index
+        System.out.println("Element at 1 index: " + linkedList.findValue(1));
+
+        // take an index by value
+        System.out.println("Index of '3' value: " + linkedList.findIndex(3));
+
+        // insert a value at given index
+        linkedList.insertAt(1, 100);
+        System.out.printf("List after 'insertAt': %s,\n", linkedList.toString());
+        System.out.println("Element at 1 index: " + linkedList.findValue(1));
     }
 }
--- src/Generics/ArrayAndLinkedListsTheory/LinkedList/LinkedList.java
+++ src/Generics/ArrayAndLinkedListsTheory/LinkedList/LinkedList.java
@@ -1,22 +1,28 @@
 package Generics.ArrayAndLinkedListsTheory.LinkedList;
 
 public class LinkedList<E> {
-    private Node<E> head;
-    private int size;
+    private Node<E> head; // the first node: [head, ...]
+    private int size; // the size of the list
 
     private static class Node<E> {
-        private E data;
-        private Node<E> next;
+        E data; // the data that the node contains
+        Node<E> next; // reference to the next node
     }
 
     public void insert(E e) {
+
+        /* create a node like 'head', where we have
+        data and do not have reference to the next node */
         Node<E> newNode = new Node<>();
         newNode.data = e;
         newNode.next = null;
 
-        if (head == null) head = newNode;
+        if (head == null) head = newNode; // if the list is empty
+
+        /* here we need to take the last node, then override the
+           'next' field, having given a reference to a new node(like 'head')*/
         else {
-            Node<E> tempNode = head;
+            Node<E> tempNode = head; // take 'head'
             while (tempNode.next != null)
                 tempNode = tempNode.next;
 
@@ -32,5 +38,103 @@ public class LinkedList<E> {
             cur = cur.next;
         }
         System.out.println("null");
+    }
+
+    @Override
+    public String toString() {
+        if (head == null) return "null";
+
+        StringBuilder result = new StringBuilder("[");
+        Node<E> cur = head;
+
+        while (cur != null) {
+            result.append(cur.data).append(", ");
+            cur = cur.next;
+        }
+        return result.substring(0, result.length() - 2) + "]";
+    }
+
+    public int size() {
+        return size;
+    }
+
+    // image there is no such field 'size'
+    public int length() {
+        Node<E> cur = head;
+        int counter = 0;
+
+        while (cur != null) {
+            counter++;
+            cur = cur.next;
+        }
+        return counter;
+    }
+
+    public void insertAtFirst(E e) {
+        /* create a new node with passed element,
+           make this node a 'head', in turn,
+           'head' node assign to 'next' field of a new node */
+        Node<E> newNode = new Node<>();
+        newNode.data = e;
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public E findValue(int index) {
+        if (index >= length()) throw new IndexOutOfBoundsException();
+
+        Node<E> cur = head;
+        int counter = 0;
+
+        while (counter != index) {
+            cur = cur.next;
+            counter++;
+        }
+        return cur.data;
+    }
+
+    public int findIndex(E value) {
+        if (head == null) return -1;
+
+        Node<E> cur = head;
+        int counter = 0;
+        while (cur != null) {
+            if (cur.data == value) return counter;
+            cur = cur.next;
+            counter++;
+        }
+        return -1;
+    }
+
+    public void insertAt(int index, E e) {
+        if (index >= length()) throw new IndexOutOfBoundsException();
+        else if (index == 0) {
+            insertAtFirst(e);
+            return;
+        }
+
+        // create a new node with passed data
+        Node<E> newNode = new Node<>();
+        newNode.data = e;
+        newNode.next = null;
+
+        // find the necessary node
+        Node<E> tempNode = head;
+        for (int i = 0; i < index - 1; i++) {
+            tempNode = tempNode.next;
+        }
+        newNode.next = tempNode.next;
+        tempNode.next = newNode;
+    }
+
+    public void reverse() {
+        if (head == null) throw new IllegalStateException("The list is empty");
+
+        Node<E> prev = null;
+        Node<E> cur = head;
+        Node<E> next;
+
+//        while ()
     }
 }
