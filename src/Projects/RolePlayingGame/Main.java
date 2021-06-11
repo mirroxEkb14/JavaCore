fddd2e796e69b8bf6c707a4d0c1b43d2f1ed9283
Daniyar Amirov <dancho.rancho07@gmail.com>
2021-06-11 19:10:41 +0600

    createHero method in main done

--- src/Projects/RolePlayingGame/Main.java
+++ src/Projects/RolePlayingGame/Main.java
@@ -1,29 +1,92 @@
 package Projects.RolePlayingGame;
 
 import Projects.RolePlayingGame.model.*;
-import Projects.RolePlayingGame.service.BattleField;
 import Projects.RolePlayingGame.util.PlayerKind;
+
+import java.util.Scanner;
 
 public class Main {
 
+    /**
+     *
+     */
+
     public static void main(String[] args) {
-        // create fantasy characters
-        Player hero = new Hero("destroyer228", PlayerKind.FORCE);
-        Monster skeleton = new Skeleton();
-        Monster goblin = new Goblin();
 
-        // place where they will fight
-        BattleField battleField = new BattleField();
+        createUserHero();
+    }
 
-        // FIGHT!
-        battleField.fight(hero, skeleton);
 
-        // delimiter
-        System.out.println("========================");
 
-        // statistics of fantasy characters after the fight
-        battleField.statisticOf(hero);
-        System.out.println();
-        battleField.statisticOf(skeleton);
+    // method asks user for nick and main stat, creates a hero
+    public static void createUserHero() {
+        Scanner scanner = new Scanner(System.in); // read user input
+        System.out.println("Role Playing Game.\nAvailable stats: Agility, Force\nExit - 'q'\n\n");
+
+        // take user name
+        System.out.println("Nick: ");
+        String userName = scanner.nextLine();
+
+        // check if user wants to leave the program
+        if (!ifExit(userName)) {
+
+            // take user selected stat
+            System.out.println("\nMain stat: ");
+            String userStat = scanner.nextLine();
+
+            // check if user wants to leave the program
+            if (!ifExit(userStat)) {
+
+                // inner circle to check user entered stat, leave it when the stat is correct
+                while (true) {
+                    // if the user entered correct stat
+                    if (checkInputStat(userStat)) {
+
+                        // create fantasy character
+                        // terminal operator -> to check what stat user chose and set it
+                        Player hero = new Hero(userName,
+                                userStat.equalsIgnoreCase("Agility") ? PlayerKind.AGILITY : PlayerKind.FORCE);
+
+                        break; // leave 'while' loop
+
+                        // if user entered unavailable stats
+                    } else {
+                        // take user input again
+                        System.out.print("\nSuch a stat is unavailable. Try again:\n- ");
+                        userStat = scanner.nextLine();
+
+                        // check if user wants to exit the program
+                        if (ifExit(userStat)) stopGame();
+                    }
+                }
+
+            // instead of entering stat user wants to stop the game
+            } else {
+                stopGame();
+            }
+
+        // instead of entering name user wants to stop the game
+        } else {
+            stopGame();
+        }
+    }
+
+    // stops the program
+    public static void stopGame() {
+        System.out.println("\nThe game is over");
+        System.exit(0);
+    }
+
+    // if user enters 'q' or 'exit' - stop the program
+    public static boolean ifExit(String userInput) {
+        return userInput.equalsIgnoreCase("q") ||
+                userInput.equalsIgnoreCase("exit");
+    }
+
+    // returns true if user entered only available stats
+    public static boolean checkInputStat(String inputStat) {
+
+        return inputStat.equalsIgnoreCase("Agility") ||
+                inputStat.equalsIgnoreCase("Force");
     }
 }
--- src/Projects/RolePlayingGame/util/Fighter.java
+++ src/Projects/RolePlayingGame/util/Fighter.java
@@ -3,7 +3,10 @@ package Projects.RolePlayingGame.util;
     /*
         Monsters and Players must attack each other,
         so we have 'Fighter' interface that says:
-        "Both Monsters and Players can fight"
+        "Both Monsters and Player can fight"
+
+        'printStatistic()' says:
+        "Both Monsters and Player can show their statistic"
      */
 
 import Projects.RolePlayingGame.model.Entity;
